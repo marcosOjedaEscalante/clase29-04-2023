9efca84168e9cd45f4c9439bdd119dd964d0ebf8
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-04-29 12:58:14 -0400

    replaceAll expresion regular trim

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,5 +21,36 @@ btnEnviar.addEventListener('click', function(){
     cajaUno.value = cajaUno.value.replaceAll('esto', `esta cadena de caracteres ${nombre}`);
     console.log(cajaUno.value);
 
+    /* expresiones regulares */
+    /* En la caja dos, todo texto que se incorpore se debe cambiar de mayúscula a minúscula
+    o viceversa */
+    var regexUno = new RegExp(/[afd]/, 'gi');
+    var regexDos = /a/gi;
+
+    cajaDos.value = cajaDos.value.replaceAll(regexUno, function(match){
+            /* return match === 'a' ? 'A' : 'a'; */
+        
+        if(match === 'a'){
+            return 'A';
+        }else if(match === 'A'){
+            return 'a';
+        }else if(match === 'd'){
+            return 'D';
+        }else if(match === 'D'){
+            return 'd';
+        }else if(match === 'f'){
+            return 'F';
+        }else if(match === 'F'){
+            return 'f';
+        }
+    });
+
+    /* trimStart . trimEnd */
+    cajaUno.value = cajaUno.value.trimEnd();
+    cajaUno.value = cajaUno.value.trimStart();
+
+    cajaUno.value = cajaUno.value.trim();
+
+
 });
 /* template string - back ticks*/
